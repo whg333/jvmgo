fb7dedec0b8d479799bfd4411e7d2570ad7c9176
whg <wanghg@skylinematrix.com>
2019-09-14 01:10:20 +0800

    first commit

--- ch01/cmd.go
+++ ch01/cmd.go
@@ -1,7 +1,35 @@
 package main
 
+import "flag"
 import "fmt"
+import "os"
 
-func main() {
-	fmt.Printf("Hello World!")
+type Cmd struct {
+	helpFlag    bool
+	versionFlag bool
+	cpOption    string
+	class       string
+	args        []string
+}
+
+func parseCmd() *Cmd {
+	cmd := &Cmd{}
+	flag.BoolVar(&cmd.helpFlag, "help", false, "print help message")
+	flag.BoolVar(&cmd.helpFlag, "?", false, "print help message")
+	flag.BoolVar(&cmd.versionFlag, "version", false, "print version and exit")
+	flag.StringVar(&cmd.cpOption, "classpath", "", "classpath")
+	flag.StringVar(&cmd.cpOption, "cp", "", "classpath")
+	flag.Usage = printUsage
+
+	args := flag.Args()
+	if len(args) > 0 {
+		cmd.class = args[0]
+		cmd.args = args[1:]
+	}
+
+	return cmd
+}
+
+func printUsage() {
+	fmt.Printf("Usage: %s [-options] class [args...]\n", os.Args[0])
 }
